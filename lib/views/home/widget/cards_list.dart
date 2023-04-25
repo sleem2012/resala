c1fd28a1cd861edf8440c2e2fd6eefe54daab6ea
forall <slem98>
2023-04-25 19:33:12 +0200

    finish

--- lib/views/home/widget/cards_list.dart
+++ lib/views/home/widget/cards_list.dart
@@ -12,11 +12,33 @@ import '../../../shared/theme/helper.dart';
 class CardsList {
   static List<CardItemsModel> cards = [
     CardItemsModel(
+
+      //Todo:add donations faces
+        image: KStorage.i.getSetting?.data?.info?.monthlyImage ?? dummyNetImg,
+        title: "أوجه التبرع",
+        btnTitle: "اعرف المزيد",
+        desc: "للخير اوجه كثيره واكيد ثواب كل خير مختلف اتبرع معانا في اوجه الخير المتعددة",
+        screen: const DonationFacesScreen()),
+
+    CardItemsModel(
         image: KStorage.i.getSetting?.data?.info?.donationImage ?? dummyNetImg,
         title: "التبرع الإلكتروني",
         btnTitle: "أضف تبرع",
         desc: "تقدر تشارك معانا في اوجه الخير المتعدده عن طريق التبرع الالكترونى",
         screen: const DigitalDonationScreen()),
+    CardItemsModel(
+        image: KStorage.i.getSetting?.data?.info?.volunteerImage ?? dummyNetImg,
+        title: "استمارة تطوع",
+        btnTitle: "اطلب استمارة",
+        desc: "التطوع بيساوى حياه خليك فرد من عيلة رساله",
+        screen: const VolunteeringFormScreen()),
+    CardItemsModel(
+        image: KStorage.i.getSetting?.data?.info?.monthlyImage ?? dummyNetImg,
+        title: "استمارة تبرع شهري",
+        btnTitle: "اطلب استمارة",
+        desc: "خير الاعمال عند الله ادومها وان قل ساهم بعمل استمارة تبرع شهرى لصالح انشطه الجمعيه",
+        screen: const MonthlyDonationScreen()),
+
     CardItemsModel(
         image: KStorage.i.getSetting?.data?.info?.mandobImage ?? dummyNetImg,
         title: "طلب مندوب تحصيل",
@@ -24,30 +46,10 @@ class CardsList {
         desc: "سواء تبرع نقدى او عينى لو داخل نطاق مدينة كفرالشيخ",
         screen: const MandopScreen()),
     CardItemsModel(
-
-        //Todo:add donations faces
-        image: KStorage.i.getSetting?.data?.info?.monthlyImage ?? dummyNetImg,
-        title: "أوجه التبرع",
-        btnTitle: "اعرف المزيد",
-        desc: "للخير اوجه كثيره واكيد ثواب كل خير مختلف اتبرع معانا في اوجه الخير المتعددة",
-        screen: const DonationFacesScreen()),
-    CardItemsModel(
         image: KStorage.i.getSetting?.data?.info?.recycleImage ?? dummyNetImg,
         title: "نشاط الريسايكل",
         btnTitle: "اعرف المزيد",
         desc: "مخلفاتك القديمه تقدر تتبرع بيها وتعمل خير وتساعد الغير",
         screen: const RecycleWrapper()),
-    CardItemsModel(
-        image: KStorage.i.getSetting?.data?.info?.monthlyImage ?? dummyNetImg,
-        title: "استمارة تبرع شهري",
-        btnTitle: "اطلب استمارة",
-        desc: "خير الاعمال عند الله ادومها وان قل ساهم بعمل استمارة تبرع شهرى لصالح انشطه الجمعيه",
-        screen: const MonthlyDonationScreen()),
-    CardItemsModel(
-        image: KStorage.i.getSetting?.data?.info?.volunteerImage ?? dummyNetImg,
-        title: "استمارة تطوع",
-        btnTitle: "اطلب استمارة",
-        desc: "التطوع بيساوى حياه خليك فرد من عيلة رساله",
-        screen: const VolunteeringFormScreen()),
   ];
 }
